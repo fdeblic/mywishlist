38bc8f70b6b5ac63e17dc2e9f4aff81f47da2a31
Elise Moreau <moreau96u@etu.univ-lorraine.fr>
2018-03-21 13:16:23 +0100

    Ajout des fichiers de bases + sujet

---
+++ src/DBException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File:  DBException.php
+ * Creation Date: 30/04/2015
+ * description:
+ *
+ * @author: canals
+ */
+
+namespace lbs\bd;
+
+
+class DBException extends \Exception { }
---
+++ src/README.md
@@ -0,0 +1,6 @@
+Contenu
+=======
+
+Ce snippet contient le démarrage du projet wishlist.
+Il se trouve à :
+git@bitbucket.org:snippets/loirotte/Aeyraz/wishlist_skel.git
