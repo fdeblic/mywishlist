9c7cebb55db312341a56bdbf4c34b80c7a770d0b
fdeblic <fdeblic@hotmail.com>
2018-04-06 17:34:42 +0200

    Création structure vue item

---
+++ src/view/ItemView.php
@@ -0,0 +1,19 @@
+<?php
+namespace mywishlist\view;
+
+  class ItemView {
+    static function render(){
+      echo 'function render()';
+    }
+
+    static function displayItems($items){
+      echo 'function displayItems()';
+    }
+
+    static function displayItem($item){
+      echo 'function displayItem()';
+    }
+
+  }
+
+?>
