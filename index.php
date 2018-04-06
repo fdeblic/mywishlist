89985dbb5551eca80201c6edf723e3f1944a4536
fdeblic <fdeblic@hotmail.com>
2018-04-06 15:22:52 +0200

    Premiers tests slim

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
+  require_once 'vendor/autoload.php';
+  $app = new \Slim\Slim();
 
-  echo "la réécriture fonctionne !"
+  $app->get('/', function() {
+    echo "Accueil";
+  });
 
+  $app->get('/test', function() {
+    echo "Test fonctionnel !<br>";
+  });
 
+  $app->get('/test/:id', function($id) {
+    echo "Test avec paramètre fonctionnel !<br>";
+    echo "Vous avez entré : $id";
+  });
+
+  $app->run();
 ?>
