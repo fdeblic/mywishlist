a1d545376fda7a4cef0f1df147c3d08f40b881e1
fdeblic <fdeblic@hotmail.com>
2018-04-06 16:57:57 +0200

    Correction configuration composer : Eloquent réinstallé

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInite4e11c9398833cf909e5125ed25b042a
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInite4e11c9398833cf909e5125ed25b042a::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiree4e11c9398833cf909e5125ed25b042a($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequiree4e11c9398833cf909e5125ed25b042a($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
