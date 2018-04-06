5ad36e977c771458be6d02c52ea97ba4b6d8d9a2
fdeblic <fdeblic@hotmail.com>
2018-04-06 15:01:35 +0200

    Installation slim 2.*

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,11 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInite4e11c9398833cf909e5125ed25b042a
 {
-    public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
         'm' => 
         array (
@@ -19,31 +14,6 @@ class ComposerStaticInite4e11c9398833cf909e5125ed25b042a
         'c' => 
         array (
             'conf\\' => 5,
-        ),
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Symfony\\Component\\Translation\\' => 30,
-        ),
-        'P' => 
-        array (
-            'Psr\\SimpleCache\\' => 16,
-            'Psr\\Container\\' => 14,
-        ),
-        'I' => 
-        array (
-            'Illuminate\\Support\\' => 19,
-            'Illuminate\\Database\\' => 20,
-            'Illuminate\\Contracts\\' => 21,
-            'Illuminate\\Container\\' => 21,
-        ),
-        'D' => 
-        array (
-            'Doctrine\\Common\\Inflector\\' => 26,
-        ),
-        'C' => 
-        array (
-            'Carbon\\' => 7,
         ),
     );
 
@@ -56,45 +26,15 @@ class ComposerStaticInite4e11c9398833cf909e5125ed25b042a
         array (
             0 => __DIR__ . '/../..' . '/src/conf',
         ),
-        'Symfony\\Polyfill\\Mbstring\\' => 
+    );
+
+    public static $prefixesPsr0 = array (
+        'S' => 
         array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Symfony\\Component\\Translation\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/translation',
-        ),
-        'Psr\\SimpleCache\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
-        ),
-        'Psr\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/container/src',
-        ),
-        'Illuminate\\Support\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/support',
-        ),
-        'Illuminate\\Database\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/database',
-        ),
-        'Illuminate\\Contracts\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/contracts',
-        ),
-        'Illuminate\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/container',
-        ),
-        'Doctrine\\Common\\Inflector\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Common/Inflector',
-        ),
-        'Carbon\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon',
+            'Slim' => 
+            array (
+                0 => __DIR__ . '/..' . '/slim/slim',
+            ),
         ),
     );
 
@@ -103,6 +43,7 @@ class ComposerStaticInite4e11c9398833cf909e5125ed25b042a
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInite4e11c9398833cf909e5125ed25b042a::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInite4e11c9398833cf909e5125ed25b042a::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInite4e11c9398833cf909e5125ed25b042a::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
