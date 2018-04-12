ff47c6970b651ac7223c9fe106191f118e583e57
Elise Moreau <moreau.elise13@gmail.com>
2018-04-12 23:30:51 +0200

    Ajout du displayItem

--- src/controller/ItemController.php
+++ src/controller/ItemController.php
@@ -1,6 +1,19 @@
 <?php
+namespace mywishlist\controller;
+require_once 'vendor/autoload.php';
+
+  use \mywishlist\models\Item as Item;
+  use \mywishlist\view\ItemView as ItemView;
+
   class ItemController{
-    
+
+      public static function displayItem($id){
+          $item = Item::where('id','=',$id)->first();
+
+          //Affiche l'item via la vue
+          $vue = new ItemView();
+          $vue->renderItem($item);
+      }
   }
 
 
