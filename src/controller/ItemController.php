084d850c21dec0a329cc3cde71f7628dab15f607
Elise Moreau <moreau.elise13@gmail.com>
2018-04-13 21:13:12 +0200

    Ajout de createItem() et editItem()

--- src/controller/ItemController.php
+++ src/controller/ItemController.php
@@ -15,22 +15,33 @@ require_once 'vendor/autoload.php';
           $vue->renderItem($item);
       }
 
-      public static function createItem(){
-        //TODO
+      public static function createItem($id){
         $view = new ItemView();
 
-        // TODO Retirer lorsque les comptes seront fonctionnels
-        $_SESSION['user_id'] = 42;
-
         // Vérifie les données envoyées
-        if (!isset($_SESSION['user_id']))  $view->notConnectedError();
-        if (!isset($_POST['item_nom']))    $view->error("veuillez entrer un nom");
-        if (!isset($_POST['item_descr']))  $view->error("veuillez entrer une description");
-        if (!isset($_POST['item_tarif']))  $view->error("veuillez entrer un tarif");
-        
+        if (!isset($_POST['item_nom'])) $view->error("veuillez entrer un nom");
+        if (!isset($_POST['item_descr'])) $view->error("veuillez entrer une description");
+        if (!filter_var($_POST['item_tarif'], FILTER_VALIDATE_FLOAT)) $view->error("Votre tarif est invalide.");
+
+        $item = new Item();
+
+        $nom = filter_var($_POST['item_nom'],FILTER_SANITIZE_STRING);
+        $descr = filter_var($_POST['item_descr'],FILTER_SANITIZE_STRING);
 
 
+        $item->liste_id = $id;
+        if (strlen($nom)> 0) $item->nom = $nom;
+        if (strlen($descr) > 0) $item->descr = $descr;
+        $item->tarif = $_POST['item_tarif'];
 
+        $item->save();
+        $view->renderItemCreated($item);
+      }
+
+      public static function editItem($item, $id){
+          //Affiche l'item via la vue
+          $view = new ItemView();
+          $view->renderFormItem($item, $id);
       }
   }
 
