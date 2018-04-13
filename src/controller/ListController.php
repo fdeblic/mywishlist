91be5e4e8af3ed4c3038c11e41a36cd49cfc2ad1
Elise Moreau <moreau.elise13@gmail.com>
2018-04-13 21:14:36 +0200

    Modification du nom de la méthode 'RenderFormulaireList' en 'RenderFormList'

--- src/controller/ListController.php
+++ src/controller/ListController.php
@@ -64,7 +64,7 @@
     public static function editList($list){
         //Affiche la liste via la vue
         $vue = new ListView();
-        $vue->renderFormulaireList($list);
+        $vue->renderFormList($list);
 
     }
   }
