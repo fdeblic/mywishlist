498bdda19fdc73bf01ff2dee23741b89a2a5e7f0
Loïc Spacher <loic.spacher@gmail.com>
2018-04-19 17:29:10 +0200

    Ajout de fonction pour le formulaire d'upload et fonction d'upload

--- src/controller/MainController.php
+++ src/controller/MainController.php
@@ -19,4 +19,34 @@
         $vue = new MainView();
         $vue->generateImgForm();
     }
+
+    public function uploadImg(){
+        $vue = new MainView();
+        $img = $_FILES['image'];
+
+        // Différents tests si l'image envoyée est correcte
+        if(!isset($img))
+            $vue->error("Image manquante");
+
+        if($img['error']!=0)
+            $vue->error("Erreur dans l'envoi de l'image");
+
+        if($img['size']>1E6) // 1Mo
+            $vue->error("Fichier trop lourd");
+
+        $allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+        $detectedType = exif_imagetype($img['tmp_name']);
+        if(!in_array($detectedType, $allowedTypes))
+            $vue->error("Le fichier n'est pas une image");
+
+        if(file_exists("./img/".$img['name']))
+            $vue->error("Le fichier est déjà présent");
+
+        if(!move_uploaded_file($img['tmp_name'],"./img/".$img['name']))
+            $vue->error("Echec de l'upload");
+
+        echo("Image bien uploadée :".$img['name']);
+        $vue->render();
+
+    }
   }
