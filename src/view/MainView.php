dd1d8d202ac675b8fc8bd83437b55f4d063f061c
Elise Moreau <moreau.elise13@gmail.com>
2018-04-21 14:05:33 +0200

    Suppression des vues relatives aux images

--- src/view/MainView.php
+++ src/view/MainView.php
@@ -8,7 +8,7 @@ namespace mywishlist\view;
 
     /* Génère le contenu HTML pour afficher une
     liste des listes passées en paramètre */
-    /*function render() {
+    function render() {
         $content = "<h1>Bienvenue en terre sainte.</h1>";
         $content .= "<p>
             Notre catalogue est à votre disposition.
@@ -18,19 +18,5 @@ namespace mywishlist\view;
         </p>";
         $_SESSION['content'] = str_replace ("\n", "\n\t", $content)."\n";
         parent::render();
-    }*/
-
-    /* Génère le contenu HTML pour la page d'upload
-    * d'une image */
-    function generateImgForm(){
-        $content = "<h1> Uploadage d'image </h1>";
-        $form = " <form action='".$_SESSION['app']->urlFor('pot_addImg_post')."' method='POST' enctype='multipart/form-data'>
-            <p>Sélectionnez une image à uploader : </p>
-            <input type='file' name='image' id='image'>
-            <input type='submit' value='Upload Image' name='submit'>
-        </form>";
-        $_SESSION['content'] = $form;
-        parent::render();
     }
-
  }
