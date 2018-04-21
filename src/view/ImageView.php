1fc1c56298e0c2df871a6f29dba01a61ef813351
Elise Moreau <moreau.elise13@gmail.com>
2018-04-21 14:05:15 +0200

    Creation d'une vue pour les images

---
+++ src/view/ImageView.php
@@ -0,0 +1,27 @@
+<?php
+namespace mywishlist\view;
+
+  class ImageView extends GlobalView {
+
+    function __construct() {
+    }
+
+    /* Génère le contenu HTML pour la page d'upload
+    * d'une image */
+    function generateImgForm(){
+        $content = "<h1> Téléchargement d'image </h1>";
+        $content .= " <form action='".$_SESSION['app']->urlFor('pot_addImg_post')."' method='POST' enctype='multipart/form-data'>
+            <p>Sélectionnez une image à télécharger : </p>
+            <input type='file' name='image' id='image'>
+            <input type='submit' value='Upload Image' name='submit'>
+        </form>";
+        $_SESSION['content'] = $content;
+        parent::render();
+    }
+
+    function renderUploadImage(){
+        $content = "<p>Image correctement téléchargée.</p>";
+        $_SESSION['content'] = $content;
+        parent::render();
+    }
+ }
