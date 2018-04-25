a7722f8bcdc16f5049855b1b56b8f49cca9ac8bc
Elise Moreau <moreau.elise13@gmail.com>
2018-04-25 23:09:59 +0200

    création du model Message

---
+++ src/models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace mywishlist\models;
+
+class Message extends \Illuminate\Database\Eloquent\Model {
+
+    protected $table = 'message_list';
+    protected $primary = 'id_message';
+    public $timestamps = true;
+
+    public function liste() {
+      return $this->belongsTo('mywishlist\models\WishList','list_id','no');
+    }
+
+    public function creator(){
+        return $this->belongsTo('mywishlist\models\Account','id_creator','id_account');
+    }
+
+}
+?>
