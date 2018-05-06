ad7428c19005e4680bb24bdb774db2d21577d192
fdeblic <fdeblic@hotmail.com>
2018-05-06 21:53:24 +0200

    Création de la classe Pot

---
+++ src/models/Pot.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace mywishlist\models;
+
+class Pot extends \Illuminate\Database\Eloquent\Model {
+
+    protected $table = 'pot_participation';
+    protected $primaryKey = 'id_pot_participation';
+    public $timestamps = false;
+
+}
+?>
