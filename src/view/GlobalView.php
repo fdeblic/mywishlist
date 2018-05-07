cdbcc6f2889e7d46c4eaa4ed7f583f637a65c5aa
fdeblic <fdeblic@hotmail.com>
2018-05-07 20:46:54 +0200

    Création de la méthode addContent(...)

--- src/view/GlobalView.php
+++ src/view/GlobalView.php
@@ -1,47 +1,53 @@
 <?php
-  namespace mywishlist\view;
+namespace mywishlist\view;
 
-  class GlobalView {
-    protected $isAdmin = false;
-    protected $HTML_PATH = "src/view/html/";
+class GlobalView {
 
-    function __construct() {
-      if (!isset($_SESSION['messages']))
-        $_SESSION['messages'] = '';
-    }
+  /*
+    Constructeur
+  */
+  function __construct() {
+    if (!isset($_SESSION['messages']))
+      $_SESSION['messages'] = '';
+    if (!isset($_SESSION['content']))
+      $_SESSION['content'] = '';
+  }
 
-    public function render() {
-      \mywishlist\controller\AccountController::generateAccountHeader();
-      $_SESSION['content'] = $_SESSION['messages'] . $_SESSION['content'];
-      include($this->HTML_PATH . 'index.php');
-      $_SESSION['content'] = "";
-      $_SESSION['messages'] = "";
-    }
+  public function render() {
+    \mywishlist\controller\AccountController::generateAccountHeader();
+    $_SESSION['content'] = $_SESSION['messages'] . $_SESSION['content'];
+    include('src/view/html/index.php');
+    $_SESSION['content'] = "";
+    $_SESSION['messages'] = "";
+  }
 
-    public function addHeadMessage($text, $type) {
-      switch ($type) {
-        case 'bad':
-          $_SESSION['messages'] .= "<p class='headMsg_bad'> $text </p>";
-        break;
-        case 'good':
-          $_SESSION['messages'] .= "<p class='headMsg_good'> $text </p>";
-        break;
-        case 'neutral':
-        default:
-          $_SESSION['messages'] .= "<p class='headMsg'> $text </p>";
-        break;
-      }
-    }
+  public function addContent($content) {
+    $_SESSION['content'] .= $content . "\r\n";
+  }
 
-    public function notConnectedError() {
-      $this->error("veuillez vous connecter...");
-    }
-
-    public function error($errorMessage) {
-      $this->addHeadMessage("Erreur : $errorMessage", "bad");
-      $_SESSION['content'] = "";
-      //$_SESSION['content'] = "<p class='errorMsg'> Erreur : $errorMessage </p>";
-      $this->render();
-      die();
+  public function addHeadMessage($text, $type) {
+    switch ($type) {
+      case 'bad':
+      $_SESSION['messages'] .= "<p class='headMsg_bad'> $text </p>";
+      break;
+      case 'good':
+      $_SESSION['messages'] .= "<p class='headMsg_good'> $text </p>";
+      break;
+      case 'neutral':
+      default:
+      $_SESSION['messages'] .= "<p class='headMsg'> $text </p>";
+      break;
     }
   }
+
+  public function notConnectedError() {
+    $this->error("veuillez vous connecter...");
+  }
+
+  public function error($errorMessage) {
+    $this->addHeadMessage("Erreur : $errorMessage", "bad");
+    $_SESSION['content'] = "";
+    $this->render();
+    die();
+  }
+}
