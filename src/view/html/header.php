41fd3d1ea66e871172111b05ca9403e28766c2ce
fdeblic <fdeblic@hotmail.com>
2018-05-21 12:47:44 +0200

    Changement de 'Catalogue' en 'Listes'

--- src/view/html/header.php
+++ src/view/html/header.php
@@ -5,10 +5,10 @@
   <!-- Menus -->
   <ul id="menu-navig">
     <li><a href="./"> Accueil </a></li>
-    <li><a href="<?php echo \Slim\Slim::getInstance()->urlFor('list_getPubLists'); ?>">Catalogue </a></li>
-    <li><a href="<?php echo \Slim\Slim::getInstance()->urlFor('list_getCreators'); ?>">Créateurs </a></li>
+    <li><a href="<?php echo \Slim\Slim::getInstance()->urlFor('list_getPubLists'); ?>"> Listes </a></li>
+    <li><a href="<?php echo \Slim\Slim::getInstance()->urlFor('list_getCreators'); ?>"> Créateurs </a></li>
     <?php if (\mywishlist\controller\AccountController::isConnected())
-      echo '<li><a href="' . \Slim\Slim::getInstance()->urlFor('acc_edit_get') . '">Mon compte</a></li>';
+      echo '<li><a href="' . \Slim\Slim::getInstance()->urlFor('acc_edit_get') . '"> Mon compte </a></li>';
     ?>
   </ul>
   <?php
