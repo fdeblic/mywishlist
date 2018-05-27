34af07e3c8b8cbfcb7fd56a90c3ddd2cd1765716
Elise Moreau <moreau.elise13@gmail.com>
2018-05-27 12:48:48 +0200

    Home message modified

--- src/view/MainView.php
+++ src/view/MainView.php
@@ -14,14 +14,22 @@ namespace mywishlist\view;
         $content = "<h1>Bienvenue en terre sainte.</h1>\n";
         $content .=
 "<p>
-    Vous souhaitez créer un liste de souhaits pour un anniversaire ou un mariage ?
+    Vous souhaitez créer une liste de souhaits pour un anniversaire ou un mariage ?
+    Ou tout événement vous tenant à coeur ?
     Vous vous trouvez au bon endroit !
-    Notre catalogue est à votre disposition.
+</p>
+<p>
+    Les listes actuelles sont disponibles dans l'onglet listes.
     Vous pouvez avoir accès aux listes publiques,
     mais également aux listes privées auxquelles vous auriez été invités.
+</p>
+<p>
     Vous pouvez bien évidemment créer vos propres listes et les partager.
     Pour cela, merci de vous connecter.
-</p>";
+</p>
+ <div align=\"center\"><i>La fête n'attend plus que vous !</i</div>
+"
+;
         $content = str_replace ("\n", "\n  ", $content);
         $this->addContent($content);
         parent::render();
